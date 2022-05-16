59a66d0464ead14975a4e4fcebc7957ef87345e5
julioxbs <julioxbs@gmail.com>
2022-05-16 15:51:54 +0100

    created methods: update and delete

--- routes/routes.js
+++ routes/routes.js
@@ -3,6 +3,9 @@ const TaskController = require("../controller/TaskController")
 
 routes.get("/", TaskController.getAllTasks);
 routes.post("/create", TaskController.createTask);
+routes.get("/getById/:id/:method", TaskController.getById);
+routes.post("/updateTask/:id", TaskController.updateTask);
+routes.get("/deleteTask/:id", TaskController.deleteTask);
 
 
 module.exports = routes
