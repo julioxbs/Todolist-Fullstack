ff36fb8dcf8a49444fb923562f8f608250c51423
julioxbs <julioxbs@gmail.com>
2022-05-18 08:55:03 +0100

    deploy configs

--- database/db.js
+++ database/db.js
@@ -1,13 +1,10 @@
 const mongoose = require('mongoose');
 
 const connectToDb = () => {
-    mongoose.connect(
-        "mongodb+srv://root:admin@todolist.dkvk6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-        {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        }
-    ).then(() => {
+    mongoose.connect(process.env.DB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }).then(() => {
         console.log("MongoDB Atlas Connected");
     }).catch((err) => console.log(err))
 }
